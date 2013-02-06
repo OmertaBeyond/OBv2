c216afc2db65291567eabaf8866470f6e68d93fc
vBm <the.vbm@gmail.com>
2013-02-06 03:35:29 +0100

    Welcome to OBv2
    Add dummy files with needed meta keys.

---
+++ ob.user.js
@@ -0,0 +1,31 @@
+// ==UserScript==
+// @name                Omerta Beyond
+// @id                  Omerta Beyond
+// @version             2.0
+// @date                06-02-2013
+// @description         Omerta Beyond 2.0 (We're back to reclaim the throne ;))
+// @homepageURL         http://www.omertabeyond.com/
+// @namespace           v4.omertabeyond.com
+// @updateURL           https://raw.github.com/OmertaBeyond/OBv2/master/beyond.meta.js
+// @supportURL          https://github.com/OmertaBeyond/OBv2/issues
+// @icon                https://raw.github.com/OmertaBeyond/OBv2/master/images/logo.small.png
+// @screenshot          https://raw.github.com/OmertaBeyond/OBv2/master/images/logo.small.png
+// @author              OBDev Team <info@omertabeyond.com>
+// @author              vBm <vbm@omertabeyond.com>
+// @author              Dopedog <dopedog@omertabeyond.com>
+// @author              Rix <rix@omertabeyond.com>
+// @author              MrWhite <mrwhite@omertabeyond.com>
+// @license             GNU General Public License v3
+// @contributionURL     https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=sbanks%40omertabeyond%2ecom&lc=GB&item_name=Omerta%20Beyond&currency_code=EUR&bn=PP%2dDonationsBF%3abtn_donateCC_LG%2egif%3aNonHosted
+// @contributionAmount  €3.00
+// @encoding            UTF-8
+// @priority            1
+// @include             http://www.omerta3.com/*
+// @include             http://omerta3.com/*
+// @include             http://www.barafranca.com/*
+// @include             http://barafranca.com/*
+// @include             http://www.barafranca.us/*
+// @include             http://barafranca.us/*
+// ==/UserScript==
+
+console.log("Welcome to OBv2 xD");
