462b51fb6988164e1e79f3ad9c1cbfe8590fcdcc
vBm <the.vbm@gmail.com>
2013-07-14 19:59:32 +0200

    Bump version to 2.0.1 So GM would hopefully give users an update uppon next check.

--- ob.meta.js
+++ ob.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name                Omerta Beyond
 // @id                  Omerta Beyond
-// @version             2.0
+// @version             2.0.1
 // @date                14-07-2013
 // @description         Omerta Beyond 2.0 (We're back to reclaim the throne ;))
 // @homepageURL         http://www.omertabeyond.com/
