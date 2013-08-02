3377f0661214d0050483d8eaec0803606a679280
vBm <the.vbm@gmail.com>
2013-08-02 02:12:26 +0200

    Bump version to force update Header re-indentation (gave a bit more space for names of resources in future)

--- ob.meta.js
+++ ob.meta.js
@@ -1,42 +1,42 @@
 // ==UserScript==
-// @name                Omerta Beyond
-// @id                  Omerta Beyond
-// @version             2.0.5
-// @date                01-08-2013
-// @description         Omerta Beyond 2.0 (We're back to reclaim the throne ;))
-// @homepageURL         http://www.omertabeyond.com/
-// @namespace           v4.omertabeyond.com
-// @updateURL           https://raw.github.com/OmertaBeyond/OBv2/master/ob.meta.js
-// @supportURL          https://github.com/OmertaBeyond/OBv2/issues
-// @icon                https://raw.github.com/OmertaBeyond/OBv2/master/images/logo.small.png
-// @screenshot          https://raw.github.com/OmertaBeyond/OBv2/master/images/logo.small.png
-// @author              OBDev Team <info@omertabeyond.com>
-// @author              vBm <vbm@omertabeyond.com>
-// @author              Dopedog <dopedog@omertabeyond.com>
-// @author              Rix <rix@omertabeyond.com>
-// @author              MrWhite <mrwhite@omertabeyond.com>
-// @license             GNU General Public License v3
-// @contributionURL     https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=sbanks%40omertabeyond%2ecom&lc=GB&item_name=Omerta%20Beyond&currency_code=EUR&bn=PP%2dDonationsBF%3abtn_donateCC_LG%2egif%3aNonHosted
-// @contributionAmount  €3.00
-// @encoding            UTF-8
-// @priority            1
-// @require             https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js
-// @require             https://ajax.googleapis.com/ajax/libs/jqueryui/1.9.1/jquery-ui.min.js
-// @resource    css     https://raw.github.com/OmertaBeyond/OBv2/master/scripts/beyond.css
-// @resource    favicon https://raw.github.com/OmertaBeyond/OBv2/master/images/favicon.png
-// @resource    logo    https://raw.github.com/OmertaBeyond/OBv2/master/images/logo.png
-// @resource    logo-old https://raw.github.com/OmertaBeyond/OBv2/master/images/logo-old.png
-// @resource    prev    https://raw.github.com/OmertaBeyond/OBv2/master/images/prev.png
-// @resource    next    https://raw.github.com/OmertaBeyond/OBv2/master/images/next.png
-// @resource    reply   https://raw.github.com/OmertaBeyond/OBv2/master/images/reply.png
-// @resource    delete  https://raw.github.com/OmertaBeyond/OBv2/master/images/delete.png
-// @resource    log     https://raw.github.com/OmertaBeyond/OBv2/master/images/changelog.png
-// @include             http://*.omerta3.com/*
-// @include             http://omerta3.com/*
-// @include             http://*.barafranca.com/*
-// @include             http://barafranca.com/*
-// @include             http://*.barafranca.nl/*
-// @include             http://barafranca.nl/*
-// @include             http://*.barafranca.us/*
-// @include             http://barafranca.us/*
+// @name                     Omerta Beyond
+// @id                       Omerta Beyond
+// @version                  2.0.6
+// @date                     02-08-2013
+// @description              Omerta Beyond 2.0 (We're back to reclaim the throne ;))
+// @homepageURL              http://www.omertabeyond.com/
+// @namespace                v4.omertabeyond.com
+// @updateURL                https://raw.github.com/OmertaBeyond/OBv2/master/ob.meta.js
+// @supportURL               https://github.com/OmertaBeyond/OBv2/issues
+// @icon                     https://raw.github.com/OmertaBeyond/OBv2/master/images/logo.small.png
+// @screenshot               https://raw.github.com/OmertaBeyond/OBv2/master/images/logo.small.png
+// @author                   OBDev Team <info@omertabeyond.com>
+// @author                   vBm <vbm@omertabeyond.com>
+// @author                   Dopedog <dopedog@omertabeyond.com>
+// @author                   Rix <rix@omertabeyond.com>
+// @author                   MrWhite <mrwhite@omertabeyond.com>
+// @license                  GNU General Public License v3
+// @contributionURL          https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=sbanks%40omertabeyond%2ecom&lc=GB&item_name=Omerta%20Beyond&currency_code=EUR&bn=PP%2dDonationsBF%3abtn_donateCC_LG%2egif%3aNonHosted
+// @contributionAmount       €3.00
+// @encoding                 UTF-8
+// @priority                 1
+// @require                  https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js
+// @require                  https://ajax.googleapis.com/ajax/libs/jqueryui/1.9.1/jquery-ui.min.js
+// @resource    css          https://raw.github.com/OmertaBeyond/OBv2/master/scripts/beyond.css
+// @resource    favicon      https://raw.github.com/OmertaBeyond/OBv2/master/images/favicon.png
+// @resource    logo         https://raw.github.com/OmertaBeyond/OBv2/master/images/logo.png
+// @resource    logo-old     https://raw.github.com/OmertaBeyond/OBv2/master/images/logo-old.png
+// @resource    prev         https://raw.github.com/OmertaBeyond/OBv2/master/images/prev.png
+// @resource    next         https://raw.github.com/OmertaBeyond/OBv2/master/images/next.png
+// @resource    reply        https://raw.github.com/OmertaBeyond/OBv2/master/images/reply.png
+// @resource    delete       https://raw.github.com/OmertaBeyond/OBv2/master/images/delete.png
+// @resource    log          https://raw.github.com/OmertaBeyond/OBv2/master/images/changelog.png
+// @include                  http://*.omerta3.com/*
+// @include                  http://omerta3.com/*
+// @include                  http://*.barafranca.com/*
+// @include                  http://barafranca.com/*
+// @include                  http://*.barafranca.nl/*
+// @include                  http://barafranca.nl/*
+// @include                  http://*.barafranca.us/*
+// @include                  http://barafranca.us/*
 // ==/UserScript==
