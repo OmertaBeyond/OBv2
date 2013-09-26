762762c6a4285fd234220f7e7e0034169137e68e
MrWhite <mrwhite@omertabeyond.com>
2013-09-27 01:32:52 +0200

    Added return to jail on self bo

--- ob.user.js
+++ ob.user.js
@@ -738,6 +738,10 @@ if (document.getElementById('game_container') !== null) {
 				bos = (bos + 1);
 				setV('bustouts', bos);
 			}
+			// Return when self bo
+			if ($('#game_container:contains(You busted yourself out of jail)').length) {
+				window.location.reload();
+			}
 			// Add amount of inmates and bustouts
 			$('table > tbody > tr > td > h1').parent().append(
 				$('<span>').text('In jail: ' + rows),
