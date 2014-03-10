a2f49fb03372b4820f18150756d65a2b3be4b5cd
Baelor <baelor.bs@gmail.com>
2014-03-10 02:07:37 +0100

    bump version to force update

--- ob.user.js
+++ ob.user.js
@@ -19,8 +19,8 @@
 // ==UserScript==
 // @name                     Omerta Beyond
 // @id                       Omerta Beyond
-// @version                  2.0.26
-// @date                     04-03-2014
+// @version                  2.0.27
+// @date                     10-03-2014
 // @description              Omerta Beyond 2.0 (We're back to reclaim the throne ;))
 // @homepageURL              http://www.omertabeyond.com/
 // @namespace                v4.omertabeyond.com
@@ -71,7 +71,7 @@ var OB_API_WEBSITE = 'http://gm.omertabeyond.com';
 var OB_NEWS_WEBSITE = 'http://news.omertabeyond.com';
 var OB_STATS_WEBSITE = 'http://stats.omertabeyond.com';
 var OB_RIX_WEBSITE = 'http://rix.omertabeyond.com';
-var OB_VERSION = '2.0.26';
+var OB_VERSION = '2.0.27';
 var cur_v = '4.5';
 
 /*
