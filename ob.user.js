OmertaBeyond/OBv2

commit e9995cdc912d7e65c8990d79a21c9e5135820731

Adds filtering cars between price X and Y

ob.user.js
@@ -3636,13 +3636,21 @@ if (document.getElementById('game_container') !== null) {
 				}).append(
 					$('<option>').attr('value', '0').text('-----'),
 					$('<option>').attr('value', '1').text('Above'),
-					$('<option>').attr('value', '2').text('Below')
-				),
+					$('<option>').attr('value', '2').text('Below'),
+					$('<option>').attr('value', '3').text('Between')
+				).on('change', function() { 
+					$('#selval2').toggle($(this).val() === '3')) 
+				}),
 				$('<input>').attr({
 					type: 'text',
 					id: 'selval',
 					size: '9'
 				}),
+				$('<input>').attr({
+					type: 'text',
+					id: 'selval2',
+					size: '9'
+				}).css('display', 'none'),
 				$('<input>').attr({
 					type: 'button',
 					id: 'selgo',
@@ -3660,6 +3668,13 @@ if (document.getElementById('game_container') !== null) {
 								check.prop('checked', false);
 							}
 							if ((carVal > val && sort == 1) || (carVal < val && sort == 2)) {
+								if (comment.indexOf('IN SAFEHOUSE') !== -1){
+									check.prop('checked', false);
+								} else {
+									check.prop('checked', true);
+								}
+							}
+							if ((carVal >= val && (carVal <= maxPrice)) && sort == 3){
 								if (comment.indexOf('IN SAFEHOUSE') !== -1){
 									check.prop('checked', false);
 								} else {

commit bb6d175b9fe0800468978b0c44e20b072b736ee7

fix syntax error in garage filter code
introduced in e9995cdc912d7e65c8990d79a21c9e5135820731

ob.user.js
@@ -3638,7 +3638,7 @@ if (document.getElementById('game_container') !== null) {
 					$('<option>').attr('value', '2').text('Below'),
 					$('<option>').attr('value', '3').text('Between')
 				).on('change', function() { 
-					$('#selval2').toggle($(this).val() === '3')) 
+					$('#selval2').toggle($(this).val() === '3');
 				}),
 				$('<input>').attr({
 					type: 'text',