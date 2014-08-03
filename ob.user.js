OmertaBeyond/OBv2

commit 024b071b4d8f8185ab2490d8819f0a646ceb10d6

Fixed favicon missing

ob.user.js
@@ -19,8 +19,8 @@
 // ==UserScript==
 // @name                     Omerta Beyond
 // @id                       Omerta Beyond
-// @version                  2.0.28
-// @date                     25-03-2014
+// @version                  2.0.29
+// @date                     28-03-2014
 // @description              Omerta Beyond 2.0 (We're back to reclaim the throne ;))
 // @homepageURL              http://www.omertabeyond.com/
 // @namespace                v4.omertabeyond.com
@@ -81,7 +81,7 @@ var OB_API_WEBSITE = 'http://gm.omertabeyond.com';
 var OB_NEWS_WEBSITE = 'http://news.omertabeyond.com';
 var OB_STATS_WEBSITE = 'http://stats.omertabeyond.com';
 var OB_RIX_WEBSITE = 'http://rix.omertabeyond.com';
-var OB_VERSION = '2.0.28';
+var OB_VERSION = GM_info.script.version;
 var cur_v = '4.6';
 
 /*
@@ -4270,7 +4270,7 @@ if (window.location.search.indexOf('action=omertician') != -1) {
 $('input[name="email"]').focus();
 
 // Replace Omerta's favicon
-$('<link rel="shortcut icon" type="image/x-icon"/>').appendTo('head').attr('href', GM_getResourceURL('favicon'));
+$('<link rel="shortcut icon" type="image/x-icon"/>').appendTo('head').attr('href', 'https://raw.github.com/OmertaBeyond/OBv2/master/images/favicon.png');
 
 /*
  * Logos replacing

ob.meta.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name                     Omerta Beyond
 // @id                       Omerta Beyond
-// @version                  2.0.28
-// @date                     25-03-2014
+// @version                  2.0.29
+// @date                     28-03-2014
 // @description              Omerta Beyond 2.0 (We're back to reclaim the throne ;))
 // @homepageURL              http://www.omertabeyond.com/
 // @namespace                v4.omertabeyond.com

commit 89d223133952eded79bb5049f73658b2902dc4de

OB doesn't even load because GM_info isn't exposed in Scriptish. - broken since 024b071b

ob.user.js
@@ -95,7 +95,7 @@ var OB_API_WEBSITE = 'http://gm.omertabeyond.com';
 var OB_NEWS_WEBSITE = 'http://news.omertabeyond.com';
 var OB_STATS_WEBSITE = 'http://stats.omertabeyond.com';
 var OB_RIX_WEBSITE = 'http://rix.omertabeyond.com';
-var OB_VERSION = GM_info.script.version;
+var OB_VERSION = '2.0.42';
 var cur_v = '4.6.2';
 
 /*