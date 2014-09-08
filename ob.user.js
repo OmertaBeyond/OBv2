23011e6abc600dd9a56b305434cdff766afdce50
MurderInc <MurderInc@faffie.biz>
2014-09-08 17:32:20 +0200

    remove redirect after buy-out since the game does the same

--- ob.user.js
+++ ob.user.js
@@ -4425,13 +4425,6 @@ $('#game_container').on('DOMNodeInserted', function(event) {
 				unsafeWindow.omerta.GUI.container.loadPage(window.location.hash.substr(1));
 			}
 		}
-		// Return when bought out
-		if ($('#game_container:contains("You bought yourself out")').length) {
-			if (!$('#bo_fired').length && nn != 'b') {
-				$('#game_container').append($('<span>').attr('id', 'bo_fired'));
-				unsafeWindow.omerta.GUI.container.loadPage(window.location.hash.substr(1));
-			}
-		}
 	}
 });
 
