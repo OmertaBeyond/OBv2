cba3dc1c39e80d1ff25a598d65438e90e0372a35
vBm <the.vbm@gmail.com>
2014-10-19 15:33:43 +0200

    Add @Brainscrewer as OUJS collaborator

--- ob.user.js
+++ ob.user.js
@@ -40,6 +40,7 @@
 // @oujs:collaborator        Gwildor
 // @oujs:collaborator        MurderInc
 // @oujs:collaborator        Sebbe
+// @oujs:collaborator        Brainscrewer
 // @license                  GNU General Public License v3
 // @contributionURL          https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=sbanks%40omertabeyond%2ecom&lc=GB&item_name=Omerta%20Beyond&currency_code=EUR&bn=PP%2dDonationsBF%3abtn_donateCC_LG%2egif%3aNonHosted
 // @contributionAmount       €3.00
