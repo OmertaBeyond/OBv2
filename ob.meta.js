ffdb13ed899024f5599032bafd97cc0c155e100a
MurderInc <MurderInc@omertabeyond.net>
2016-01-11 15:47:19 +0100

    release v2.0.66

--- ob.meta.js
+++ ob.meta.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name                     Omerta Beyond
 // @id                       Omerta Beyond
-// @version                  2.0.65
-// @date                     03-09-2015
+// @version                  2.0.66
+// @date                     11-01-2016
 // @description              Omerta Beyond 2.0 (We're back to reclaim the throne ;))
 // @homepageURL              http://www.omertabeyond.net/
 // @namespace                v4.omertabeyond.com
