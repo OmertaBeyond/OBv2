e4ee38d5641668358ded905ca45fcccd2f45c93b
MurderInc <MurderInc@omertabeyond.net>
2019-04-14 21:50:32 +0200

    Bump version to 2.2.7

--- ob.meta.js
+++ ob.meta.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name                     Omerta Beyond
 // @id                       Omerta Beyond
-// @version                  2.2.6
-// @date                     25-02-2019
+// @version                  2.2.7
+// @date                     14-04-2019
 // @description              Omerta Beyond 2.2 (We're back to reclaim the throne ;))
 // @homepageURL              https://www.omertabeyond.net/
 // @namespace                v4.omertabeyond.com
